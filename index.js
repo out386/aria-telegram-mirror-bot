58de2eba3224e43d34ac9032249110169ce0544f
Ritayan Chakraborty <ritayanout@gmail.com>
2018-05-15 16:44:29 +0530

    Auto delete status messages

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const downloadUtils = require('./download_tools/utils.js');
 const dlVars = require('./download_tools/vars.js');
 const ariaTools = require('./download_tools/aria-tools.js');
 const constants = require('./.constants.js');
+const msgTools = require('./msg-tools.js');
 
 const options = {
   polling: true
@@ -13,81 +14,66 @@ var websocketOpened = false;
 initAria2();
 
 bot.onText(/^\/mirror (.+)/i, (msg, match) => {
-  var authorizedCode = isAuthorized(msg);
+  var authorizedCode = msgTools.isAuthorized(msg);
   if (authorizedCode > -1) {
     if (websocketOpened) {
       if (dlVars.isDownloading) {
-        bot.sendMessage(msg.chat.id,
-          dlVars.tgUsername + ' is mirroring something. Please wait.',
-          {reply_to_message_id: msg.message_id}
-        );
+        sendMessage(msg, dlVars.tgUsername + ' is mirroring something. Please wait.');
       } else {
         download(msg, match[1]);
       }
     } else {
-      bot.sendMessage(msg.chat.id,
-        'Websocket isn\'t open. Can\'t download',
-        {reply_to_message_id: msg.message_id}
-      );
+      sendMessage(msg, 'Websocket isn\'t open. Can\'t download');
     }
   } else {
-    bot.sendMessage(msg.chat.id, 'You cannot use this bot here.', {reply_to_message_id: msg.message_id});
+    sendMessage(msg, 'You cannot use this bot here.');
   }
 });
 
 bot.onText(/^\/mirrorStatus/i, (msg) => {
-  var authorizedCode = isAuthorized(msg);
+  var authorizedCode = msgTools.isAuthorized(msg);
   if (authorizedCode > -1) {
     if (dlVars.isDownloading) {
       if (dlVars.isUploading) {
-        bot.sendMessage(msg.chat.id, 'Upload in progress.', {
-          reply_to_message_id: msg.message_id
-        });
+        sendMessage(msg, 'Upload in progress.');
       } else {
         ariaTools.getStatus(dlVars.downloadGid, (err, message) => {
           if (!err) {
-            bot.sendMessage(msg.chat.id, message, {
-              reply_to_message_id: msg.message_id,
-              parse_mode: 'HTML'
-            });
+            sendMessage(msg, message);
           } else {
             console.log('status: ', err);
           }
         });
       }
     } else {
-      bot.sendMessage(msg.chat.id, 'No active downloads', {
-        reply_to_message_id: msg.message_id
-      });
+      sendMessage(msg, 'No active downloads');
     }
   } else {
-    bot.sendMessage(msg.chat.id, 'You cannot use this bot here.', {reply_to_message_id: msg.message_id});
+    sendMessage(msg, 'You cannot use this bot here.');
   }
 });
 
 bot.onText(/^\/cancelMirror/i, (msg) => {
-  var authorizedCode = isAuthorized(msg);
+  var authorizedCode = msgTools.isAuthorized(msg);
   if (authorizedCode > -1 && authorizedCode < 3) {
     cancelMirror(msg);
   } else if (authorizedCode === 3) {
-    isAdmin(msg, (e, res) => {
+    msgTools.isAdmin(bot, msg, (e, res) => {
       if (res) {
         cancelMirror(msg);
       } else {
-        bot.sendMessage(msg.chat.id, 'You do not have the permission to do that.', {reply_to_message_id: msg.message_id});
+        sendMessage(msg, 'You do not have the permission to do that.');
       }
     });
   } else {
-    bot.sendMessage(msg.chat.id, 'You cannot use this bot here.', {reply_to_message_id: msg.message_id});
+    sendMessage(msg, 'You cannot use this bot here.');
   }
 });
 
 function cancelMirror (msg) {
   if (dlVars.isDownloading) {
     if (dlVars.isUploading) {
-      bot.sendMessage(msg.chat.id, 'Upload in progress. Cannot cancel.', {
-        reply_to_message_id: msg.message_id
-      });
+      sendMessage(msg, 'Upload in progress. Cannot cancel.');
     } else {
       ariaTools.stopDownload(dlVars.downloadGid, () => {
       // Not sending a message here, because a cancel will fire
@@ -95,18 +81,14 @@ function cancelMirror (msg) {
       // person who started the download
 
         if (dlVars.tgChatId !== msg.chat.id) {
-        // Notify if this is not the chat the download started in
-          bot.sendMessage(msg.chat.id, 'The download was canceled.', {
-            reply_to_message_id: msg.message_id
-          });
+          // Notify if this is not the chat the download started in
+          sendMessage(msg, 'The download was canceled.');
         }
         downloadUtils.cleanupDownload();
       });
     }
   } else {
-    bot.sendMessage(msg.chat.id, 'No download in progress.', {
-      reply_to_message_id: msg.message_id
-    });
+    sendMessage(msg, 'No download in progress.');
   }
 }
 
@@ -116,7 +98,7 @@ function download (msg, match) {
     (err, gid) => {
       if (err) {
         console.log('Failure', err);
-        sendMessage('Failed to start the download. ' + err['message']);
+        sendMessageReplyOriginal('Failed to start the download. ' + err['message']);
         downloadUtils.cleanupDownload();
       } else {
         console.log('download: ' + match + ' gid: ' + gid);
@@ -124,15 +106,24 @@ function download (msg, match) {
     });
 }
 
-function sendMessage (message) {
+function sendMessage (msg, message) {
+  bot.sendMessage(msg.chat.id, message, {
+    reply_to_message_id: msg.message_id,
+    parse_mode: 'HTML'
+  })
+    .then((res) => {
+      msgTools.deleteMsg(bot, res);
+      msgTools.deleteMsg(bot, msg);
+    })
+    .catch((ignored) => {});
+}
+
+function sendMessageReplyOriginal (message) {
   bot.sendMessage(dlVars.tgChatId, message, {
     reply_to_message_id: dlVars.tgMessageId,
     parse_mode: 'HTML'
   })
-    .then((res) => {})
-    .catch((err) => {
-      console.log('sendMessage:' + JSON.stringify(err, null, 2));
-    });
+    .catch((ignored) => {});
 }
 
 function initAria2 () {
@@ -151,18 +142,18 @@ function initAria2 () {
     dlVars.isUploading = false;
     dlVars.downloadGid = gid;
     console.log('start', gid);
-    sendMessage('Download started.');
+    sendMessageReplyOriginal('Download started.');
   });
   ariaTools.setOnDownloadStop((gid) => {
     console.log('stop', gid);
-    sendMessage('Download stopped.');
+    sendMessageReplyOriginal('Download stopped.');
     downloadUtils.cleanupDownload();
   });
   ariaTools.setOnDownloadComplete((gid) => {
     ariaTools.getAriaFilePath(gid, (err, file) => {
       if (err) {
         console.log('onDownloadComplete: tellStatus: ' + JSON.stringify(err, null, 2));
-        sendMessage('Upload failed. Could not get downloaded files.');
+        sendMessageReplyOriginal('Upload failed. Could not get downloaded files.');
         downloadUtils.cleanupDownload();
         return;
       }
@@ -176,7 +167,7 @@ function initAria2 () {
   });
   ariaTools.setOnDownloadError((gid) => {
     console.log('error', gid);
-    sendMessage('Download error.');
+    sendMessageReplyOriginal('Download error.');
     downloadUtils.cleanupDownload();
   });
 }
@@ -184,35 +175,8 @@ function initAria2 () {
 function driveUploadCompleteCallback (err, url, filePath, fileName) {
   if (err) {
     console.log('uploadFile: ' + filePath + ': ' + JSON.stringify(err, null, 2));
-    sendMessage('Failed to upload <code>' + fileName + '</code> to Drive');
+    sendMessageReplyOriginal('Failed to upload <code>' + fileName + '</code> to Drive');
   } else {
-    sendMessage('<a href=\'' + url + '\'>' + fileName + '</a>');
+    sendMessageReplyOriginal('<a href=\'' + url + '\'>' + fileName + '</a>');
   }
-}
-
-function isAuthorized (msg) {
-  for (var i = 0; i < constants.SUDO_USERS.length; i++) {
-    if (constants.SUDO_USERS[i] === msg.from.id) return 0;
-  }
-  if (dlVars.isDownloading && msg.from.id === dlVars.tgFromId) return 1;
-  if (constants.AUTHORIZED_CHATS.indexOf(msg.chat.id) > -1 &&
-    msg.chat.all_members_are_administrators) return 2;
-  if (constants.AUTHORIZED_CHATS.indexOf(msg.chat.id) > -1) return 3;
-  return -1;
-}
-
-function isAdmin (msg, callback) {
-  bot.getChatAdministrators(msg.chat.id)
-    .then(members => {
-      for (var i = 0; i < members.length; i++) {
-        if (members[i].user.id === msg.from.id) {
-          callback(null, true);
-          return;
-        }
-      }
-      callback(null, false);
-    })
-    .catch(() => {
-      callback(null, false);
-    });
 }
---
+++ msg-tools.js
@@ -0,0 +1,43 @@
+const dlVars = require('./download_tools/vars.js');
+const constants = require('./.constants.js');
+
+async function deleteMsg (bot, msg) {
+  await sleep(5000);
+  bot.deleteMessage(msg.chat.id, msg.message_id)
+    .catch(ignored => {});
+}
+function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function isAuthorized (msg) {
+  for (var i = 0; i < constants.SUDO_USERS.length; i++) {
+    if (constants.SUDO_USERS[i] === msg.from.id) return 0;
+  }
+  if (dlVars.isDownloading && msg.from.id === dlVars.tgFromId) return 1;
+  if (constants.AUTHORIZED_CHATS.indexOf(msg.chat.id) > -1 &&
+    msg.chat.all_members_are_administrators) return 2;
+  if (constants.AUTHORIZED_CHATS.indexOf(msg.chat.id) > -1) return 3;
+  return -1;
+}
+
+function isAdmin (bot, msg, callback) {
+  bot.getChatAdministrators(msg.chat.id)
+    .then(members => {
+      for (var i = 0; i < members.length; i++) {
+        if (members[i].user.id === msg.from.id) {
+          callback(null, true);
+          return;
+        }
+      }
+      callback(null, false);
+    })
+    .catch(() => {
+      callback(null, false);
+    });
+}
+
+module.exports.deleteMsg = deleteMsg;
+module.exports.sleep = sleep;
+module.exports.isAuthorized = isAuthorized;
+module.exports.isAdmin = isAdmin;
