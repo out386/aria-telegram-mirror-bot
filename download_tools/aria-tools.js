out386/aria-telegram-mirror-bot

commit 4ab7378768b1a597b812c162e1514a0e74d059e7

Fix freeze when download contains no files
Yes, I know that these lines should get pulled into a separate function, but I'm lazy.

index.js
@@ -419,6 +419,12 @@ function initAria2 () {
         });
       } else {
         console.log('onDownloadComplete: No files');
+        var reason = 'Upload failed. Could not get files.';
+        sendMessageReplyOriginal(reason);
+        updateAllStatus(reason);
+        deleteOrigReply();
+        msgTools.notifyExternal(false, gid, dlVars.tgChatId);
+        downloadUtils.cleanupDownload();
       }
     });
   });

commit e064051d5b7be82a9165c629af24d59e488b8aa8

Fixes #15 and bug introduced by 4ab7378768
4ab7378768 had broken the ability to download Metalinks and BitTorrents.

download_tools/aria-tools.js
@@ -94,6 +94,21 @@ function getStatus (gid, callback) {
     });
 }
 
+function isDownloadMetadata (gid, callback) {
+  aria2.tellStatus(gid, ['followedBy'], (err, res) => {
+    if (err) {
+      callback(err);
+      console.log('ERROR:', err);
+    } else {
+      if (res['followedBy']) {
+        callback(null, true);
+      } else {
+        callback(null, false);
+      }
+    }
+  });
+}
+
 function getFileSize (gid, callback) {
   aria2.tellStatus(gid,
     ['totalLength'],
@@ -189,3 +204,4 @@ module.exports.addUri = addUri;
 module.exports.getStatus = getStatus;
 module.exports.stopDownload = stopDownload;
 module.exports.getFileSize = getFileSize;
+module.exports.isDownloadMetadata = isDownloadMetadata;

index.js
@@ -330,6 +330,16 @@ function deleteOrigReply () {
   }
 }
 
+function cleanupDownload (gid, message) {
+  clearInterval(statusInterval);
+  statusInterval = null;
+  sendMessageReplyOriginal(message);
+  updateAllStatus(message);
+  deleteOrigReply();
+  msgTools.notifyExternal(false, gid, dlVars.tgChatId);
+  downloadUtils.cleanupDownload();
+}
+
 function initAria2 () {
   ariaTools.openWebsocket((err) => {
     if (err) {
@@ -378,28 +388,17 @@ function initAria2 () {
     ariaTools.getAriaFilePath(gid, (err, file) => {
       if (err) {
         console.log('onDownloadComplete: ' + JSON.stringify(err, null, 2));
-        clearInterval(statusInterval);
-        statusInterval = null;
         var message = 'Upload failed. Could not get downloaded files.';
-        sendMessageReplyOriginal(message);
-        updateAllStatus(message);
-        deleteOrigReply();
-        msgTools.notifyExternal(false, gid, dlVars.tgChatId);
-        downloadUtils.cleanupDownload();
+        cleanupDownload(gid, message);
         return;
       }
+
       if (file) {
         ariaTools.getFileSize(gid, (err, size) => {
           if (err) {
             console.log('onDownloadComplete: ' + JSON.stringify(err, null, 2));
-            clearInterval(statusInterval);
-            statusInterval = null;
             var message = 'Upload failed. Could not get file size.';
-            sendMessageReplyOriginal(message);
-            updateAllStatus(message);
-            deleteOrigReply();
-            msgTools.notifyExternal(false, gid, dlVars.tgChatId);
-            downloadUtils.cleanupDownload();
+            cleanupDownload(gid, message);
             return;
           }
 
@@ -410,21 +409,26 @@ function initAria2 () {
             ariaTools.uploadFile(file, size, driveUploadCompleteCallback);
           } else {
             var reason = 'Upload failed. Blacklisted file name.';
-            sendMessageReplyOriginal(reason);
-            updateAllStatus(reason);
-            deleteOrigReply();
-            msgTools.notifyExternal(false, gid, dlVars.tgChatId);
-            downloadUtils.cleanupDownload();
+            cleanupDownload(gid, reason);
           }
         });
       } else {
-        console.log('onDownloadComplete: No files');
-        var reason = 'Upload failed. Could not get files.';
-        sendMessageReplyOriginal(reason);
-        updateAllStatus(reason);
-        deleteOrigReply();
-        msgTools.notifyExternal(false, gid, dlVars.tgChatId);
-        downloadUtils.cleanupDownload();
+        ariaTools.isDownloadMetadata(gid, (err, isMetadata) => {
+          if (err) {
+            console.log('onDownloadComplete: isMetadata: ' + JSON.stringify(err, null, 2));
+            var message = 'Upload failed. Could not check if the file is metadata.';
+            cleanupDownload(gid, message);
+            return;
+          }
+
+          if (isMetadata) {
+            console.log('onDownloadComplete: No files');
+          } else {
+            console.log('onDownloadComplete: No files - not metadata.');
+            var reason = 'Upload failed. Could not get files.';
+            cleanupDownload(gid, reason);
+          }
+        });
       }
     });
   });