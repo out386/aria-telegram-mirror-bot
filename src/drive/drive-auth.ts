dc4628d8936fc94f9a0506929a85dc6881498ddf
Ritayan Chakraborty <ritayanout@gmail.com>
2020-03-24 11:31:16 +0530

    ckpt

--- src/drive/drive-auth.ts
+++ src/drive/drive-auth.ts
@@ -2,9 +2,12 @@ import fs = require('fs');
 import readline = require('readline');
 import { google } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
+import http = require('http');
+import url = require('url');
 
 const SCOPE = 'https://www.googleapis.com/auth/drive';
 const TOKEN_PATH = './credentials.json';
+const AUTH_LISTEN_URL = 'http://localhost:6590/tg-m-bot-oauth';
 
 /**
  * Authorize a client with credentials, then call the Google Drive API.
@@ -29,18 +32,30 @@ export function call(callback: (err: string, client: OAuth2Client) => void): voi
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials: any, callback: (err: string, client: OAuth2Client) => void): void {
-  const clientSecret = credentials.installed.client_secret;
-  const clientId = credentials.installed.client_id;
-  const redirectUris = credentials.installed.redirect_uris;
-  const oAuth2Client = new google.auth.OAuth2(
-    clientId, clientSecret, redirectUris[0]);
+  const creds = credentials.web;
+  if (creds) {
+    const invalidRedirectUri = `Missing redirect_uris, or wrong endpoint. While creating a client ID in the
+      Cloud Console, make sure you add "http://localhost/tg-m-bot-oauth:6590" in the Authorized redirect URIs section.`;
+    const clientSecret = creds.client_secret;
+    const clientId = creds.client_id;
+    const redirectUris = creds.redirect_uris;
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, 'utf8', (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(null, oAuth2Client);
-  });
+    if (redirectUris && redirectUris.length > 0 && redirectUris[0] === AUTH_LISTEN_URL) {
+      const oAuth2Client = new google.auth.OAuth2(
+        clientId, clientSecret, redirectUris[0]);
+
+      // Check if we have previously stored a token.
+      fs.readFile(TOKEN_PATH, 'utf8', (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(null, oAuth2Client);
+      });
+    } else {
+      throw new Error(invalidRedirectUri);
+    }
+  } else {
+    throw new Error('Malformed client_secret.json. Please check the the README and client_secret.json');
+  }
 }
 
 /**
@@ -59,17 +74,35 @@ function getAccessToken(oAuth2Client: OAuth2Client, callback: (err: string, clie
     input: process.stdin,
     output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err.message, null);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(null, oAuth2Client);
-    });
-  });
+
+
+  const server = http
+    .createServer(async (req, res) => {
+      if (req.url.indexOf('/tg-m-bot-oauth') > -1) {
+        const qs = new url.URL(req.url, AUTH_LISTEN_URL)
+          .searchParams;
+        if (qs && qs.get('code')) {
+          res.end(
+            'Authentication successful! Please return to the console.'
+          );
+          server.close();
+
+          const tokenResp = await oAuth2Client.getToken(qs.get('code'));
+          console.log(tokenResp.tokens);
+          oAuth2Client.setCredentials(tokenResp.tokens);
+          // Store the token to disk for later program executions
+          fs.writeFile(TOKEN_PATH, JSON.stringify(tokenResp.tokens), (err) => {
+            if (err) console.error(err.message);
+            console.log('Token stored to', TOKEN_PATH);
+          });
+          callback(null, oAuth2Client);
+        } else {
+          res.end('Invalid code');
+        }
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    }).listen(6590, "localhost", () => console.log("Waiting for code"));
+
 }
